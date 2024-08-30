beadd83cd3557ebf4a5cc02ae70ede68344e47e8
Diego Fernando Angel Londono <diego.angel.londono@gmail.com>
2024-08-30 17:18:24 -0300

    feat: permutation solution

--- coding-challenges/cracking-the-coding-interview/problems/02_checkPermutations.ts
+++ coding-challenges/cracking-the-coding-interview/problems/02_checkPermutations.ts
@@ -4,5 +4,18 @@
 // [LeetCode](https://leetcode.com/problems/permutation-in-string/)
 
 export default function checkPermutations (s1 : string, s2: string): boolean {
+  if (s1.length !== s2.length) return false
 
+  const s2List = s2.split('')
+  for (const char of s1) {
+    const index = s2List.indexOf(char)
+    if (index === -1) return false
+    s2List.splice(index, 1)
+  }
+
+  return !s2List.length
+  
+  /* sorting strings solution
+    return s1.split('').sort().join('') === s2.split('').sort().join('')
+  */
 }
