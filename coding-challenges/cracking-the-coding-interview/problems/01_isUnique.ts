e23d94e3acca3082dc42b5e6ceb067733098e19b
Diego Fernando Angel Londono <diego.angel.londono@gmail.com>
2024-08-30 17:03:27 -0300

    feat: is unique

--- coding-challenges/cracking-the-coding-interview/problems/01_isUnique.ts
+++ coding-challenges/cracking-the-coding-interview/problems/01_isUnique.ts
@@ -3,4 +3,18 @@
 // Implement an algorithm to determine if a string has all unique characters.
 // What if you cannot use additional data structures?
 
-export default function isUnique(str: string): boolean {}
+export default function isUnique(str: string): boolean {
+  /* Looping
+  const chars = {}
+  for (const char of str) {
+    if (chars[char]) {
+      return false
+    }
+
+    chars[char] = 1
+  }
+
+  return true
+  */
+  return Array.from(new Set(str.split(''))).length === str.length
+}
