67ed32c0d9f4201bbf66798aadea7011276661db
Diego Fernando Angel Londono <diego.angel.londono@gmail.com>
2024-09-01 15:55:19 -0300

    feature: zero matrix solution

--- coding-challenges/cracking-the-coding-interview/problems/08_zeroMatrix.ts
+++ coding-challenges/cracking-the-coding-interview/problems/08_zeroMatrix.ts
@@ -5,5 +5,30 @@
 type Matrix = number[][]
 
 export default function zeroMatrix (matrix: Matrix) {
+  const n = matrix.length
+  const m = matrix[0].length
+  const colMarkers: number[] = []
 
+  for (let i = 0; i < n; i++) {
+    const row = matrix[i] 
+    let marker = false
+    for (let j = 0; j < m; j++) {
+      if (row[j] === 0) {
+        colMarkers.push(j)
+        marker = true
+      }
+    }
+
+    if (marker) {
+      matrix[i] = Array(m).fill(0)
+    }
+  }
+
+  for (const j of colMarkers) {
+    for (let i = 0; i < n; i++) {
+      matrix[i][j] = 0
+    }
+  }
+
+  return matrix
 }
