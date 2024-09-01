ca7e6fae9b1aabc2346fbc22d5b121dd86981eee
Diego Fernando Angel Londono <diego.angel.londono@gmail.com>
2024-09-01 09:34:14 -0300

    feature: one away solution

--- coding-challenges/cracking-the-coding-interview/problems/05_oneAway.ts
+++ coding-challenges/cracking-the-coding-interview/problems/05_oneAway.ts
@@ -5,5 +5,28 @@
 // Given two strings, write a function to check if they are one edit (or zero edits) away.
 
 export default function isOneAway(str1: string, str2: string): boolean {
-  
+  if (!str1.length && str2.length === 1) {
+    return true
+  } else if (Math.abs(str1.length - str2.length) > 1) {
+    return false
+  }
+
+  let difference = str1.length === str2.length ? 0 : 1;
+  for (let i = 0; i < str1.length; i++) {
+    if (
+      str1[i] === str2[i] ||
+      (str1.length < str2.length && str1[i] === str2[i + 1]) ||
+      (str1.length > str2.length && str1[i + 1] === str2[i])
+    ) {
+      continue
+    } else if (str1[i] !== str2[i]) {
+      difference += 1
+    }
+
+    if (difference > 1) {
+      return false
+    }
+  }
+
+  return true
 }
