27e8fe61cd07c622cb23068ab71cada575ed4ea8
Diego Fernando Angel Londono <diego.angel.londono@gmail.com>
2024-09-01 10:05:12 -0300

    feature: string compression solution

--- coding-challenges/cracking-the-coding-interview/problems/06_stringCompression.ts
+++ coding-challenges/cracking-the-coding-interview/problems/06_stringCompression.ts
@@ -7,5 +7,18 @@
 // You can assume the string has only uppercase and lowercase letters (a - z).
 
 export default function stringCompression (str: string) : string {
- 
+  let compressed: (string | number)[] = [str[0], 0]
+  let i = 0
+  for (const char of str) {
+    if (char !== compressed[i]) {
+      i += 2
+      compressed[i] = char
+      compressed[i + 1] = 0
+    }
+    (compressed[i + 1] as number) += 1
+  }
+
+  return compressed.length >= str.length
+    ? str
+    : compressed.join('')
 }
